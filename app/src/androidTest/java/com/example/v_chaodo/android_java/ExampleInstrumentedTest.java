2ee0185da527005ac292f8e9622f944a58628b17
Chaoyun1234 <dongchaoyun01>
2017-08-30 17:36:07 +0800

    AddUpdate Distribute

--- app/src/androidTest/java/com/example/v_chaodo/android_java/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/v_chaodo/android_java/ExampleInstrumentedTest.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
+
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +21,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.v_chaodo.android_java", appContext.getPackageName());
+    }
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
     }
 }
